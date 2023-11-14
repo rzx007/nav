d030e989811e527056ae34b47168c2f6106ecee9
rzx100 <770304867@qq.com>
2023-11-14 16:31:44 +0800

    feat: 添加快速链接

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -7,6 +7,10 @@ export const nav: DefaultTheme.NavItem[] = [
     link: 'https://wangchujiang.com/reference/index.html'
   },
   {
+    text: 'vue3-resource',
+    link: 'https://hu-snail.github.io/vue3-resource/'
+  },
+  {
     text: '日常笔记',
     link: 'https://rzx007.github.io/docs/'
   }
