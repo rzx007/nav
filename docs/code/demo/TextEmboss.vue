08d9d48932a00c10971b76d6901b2db6af7bd580
ruanzhixiong <ruanzhixiong@bobandata.com>
2025-04-05 21:29:43 +0800

    docs(html-dom): 添加文字浮雕效果文档
    - 在菜单中添加文字浮雕效果的链接
    - 新增 text-emboss.md 文件，介绍文字浮雕效果的实现方法

---
+++ docs/code/demo/TextEmboss.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { ref } from 'vue'
+
+const text = ref('Hello World')
+</script>
+
+<template>
+  <div class="text-emboss">
+    <h1>{{ text }}</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.text-emboss {
+  background-color: #eee;
+  h1 {
+    font-size: 21vmin;
+    font-family: 'Luckiest Guy';
+    line-height: 1;
+    margin: 0;
+    letter-spacing: 5px;
+    color:#e6e6e6;
+    text-align: center;
+    text-shadow: 1px -1px #fff, 2px -2px #fff, -1px 1px #999, -2px 2px #999;
+  }
+}
+</style>
---
+++ docs/html-dom/text-emboss.md
@@ -0,0 +1,14 @@
+# 文字浮雕效果
+
+::: details 点击查看代码
+<<< @/code/demo/TextEmboss.vue{vue}
+:::
+
+
+### 效果
+
+<script setup>
+import textEmboss from '../code/demo/TextEmboss.vue'
+</script>
+
+<textEmboss />
